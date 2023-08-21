61ee951adc31ea4e662db53cf813edd32fafc639
bhagvant-phadnis <bhagvant.phadnis@gmail.com>
2023-08-21 23:54:09 +0200

    Addes custom user profile model, manager and migrations

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -115,9 +115,11 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
