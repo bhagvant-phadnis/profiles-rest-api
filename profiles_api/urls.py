9d62ec79251ad113d4bfead55381d185d27c5feb
bhagvant-phadnis <bhagvant.phadnis@gmail.com>
2023-08-25 16:46:22 +0200

    Added user profile fees api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -4,8 +4,9 @@ from profiles_api import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('hello-viewset',views.HelloViewSet,base_name='hello-viewset')
-router.register('profile',views.UserProfileViewSet)
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloAPIView.as_view()),
